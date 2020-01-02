8bf88d02981ea9abafeb9cb524b74873338c6d72
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-01-02 17:19:39 +0300

    Initial commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/landingpage.scss';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div className="landing-page-container">
+        <h1>Elevate</h1>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
