b5e372f81539b640575e003c5c6c1a8f5c105bec
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-01-13 18:28:54 +0300

    Add dots for the slides

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,9 @@ class Home extends React.Component {
     super(props);
     this.state = {
       slideIndex: 1,
-      slides: document.getElementsByClassName('slides')
+      slides: document.getElementsByClassName('slides'),
+      dots: document.getElementsByClassName('dot'),
+      tooltips: document.getElementsByClassName('tool-tip')
     };
     this.timeoutFunction = null;
   }
@@ -23,7 +25,7 @@ class Home extends React.Component {
   };
   showSlides = slideIndex => {
     let i;
-    const { slides } = this.state;
+    const { slides, dots } = this.state;
     if (slideIndex > slides.length) {
       slideIndex = 1;
       this.setState({
@@ -40,9 +42,13 @@ class Home extends React.Component {
       slides[i].classList.remove('full-height');
       slides[i].classList.add('zero-height');
     }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace('active', '');
+    }
     slides[slideIndex - 1].classList.add('full-height');
     slides[slideIndex - 1].classList.remove('zero-height');
-    this.handleAutoPlay(5000);
+    dots[slideIndex - 1].className += ' active';
+    // this.handleAutoPlay(5000);
   };
 
   gotoNext = () => {
@@ -58,22 +64,49 @@ class Home extends React.Component {
       return { slideIndex: prevState.slideIndex - 1 };
     }, this.showSlides(this.state.slideIndex));
   };
-
+  handleDotOnHover = index => {
+    const { tooltips } = this.state;
+    tooltips[index].className += ' show';
+  };
+  handleDotOnHoverAway = index => {
+    const { tooltips } = this.state;
+    tooltips[index].className = tooltips[index].className.replace('show', '');
+  };
+  handleSlideChange = index => {
+    clearTimeout(this.timeoutFunction);
+    this.showSlides(index + 1);
+  };
   render() {
     return (
       <div className="home-container">
         <Header />
         <Menu />
-        <div className="landing-content-container slide-1 slides" ref={element => (this.slideOne = element)}>
+        <div className="dots-container">
+          {[1, 2, 3, 4].map((dot, index) => (
+            <div key={index} className="dot-container">
+              <div
+                className="dot"
+                onMouseOver={() => this.handleDotOnHover(index)}
+                onClick={() => this.handleSlideChange(index)}
+                onMouseOut={() => this.handleDotOnHoverAway(index)}
+              ></div>
+              <div className="tool-tip">
+                <div className="arrow-left"></div>
+                <div className="tool-tip-text">Slide</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="landing-content-container slide-1 slides">
           <h1>Branding</h1>
         </div>
-        <div className="landing-content-container slide-2 slides" ref={element => (this.slideTwo = element)}>
+        <div className="landing-content-container slide-2 slides">
           <h1>Digital</h1>
         </div>
-        <div className="landing-content-container slide-3 slides" ref={element => (this.slideThree = element)}>
+        <div className="landing-content-container slide-3 slides">
           <h1>Btl</h1>
         </div>
-        <div className="landing-content-container slide-4 slides" ref={element => (this.slideThree = element)}>
+        <div className="landing-content-container slide-4 slides">
           <h1>Something</h1>
         </div>
         <button className="prev" onClick={() => this.gotoPrev()}>
