fd288db1a98c574c41d9c437988323b8a1f84a6c
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-01-31 15:40:41 +0300

    Handle mouse movement for buld

--- src/components/slides/SlideOne.jsx
+++ src/components/slides/SlideOne.jsx
@@ -5,17 +5,30 @@ import { bulbImage, brandingTextImage, smallBulb } from '../../assets';
 export default function SlideOne() {
   const bulbImageContainer = useRef(null);
   const brandingText = useRef(null);
+  const bulbImageRef = useRef(null);
+  const slideOneContent = useRef(null);
 
   useEffect(() => showBulb());
+  useEffect(() => handleMouseMovement());
   function showBulb() {
     setTimeout(() => {
       brandingText.current.className += ' show-branding-text';
       bulbImageContainer.current.className += ' show-bulb';
     }, 1200);
   }
+
+  console.log(slideOneContent);
+
+  function handleMouseMovement() {
+    slideOneContent.current.addEventListener('mousemove', e => {
+      bulbImageRef.current.style.cssText = `
+      transform: translate(-${e.offsetX / 20}px, -${e.offsetY / 20}px) rotate(0deg);
+      `;
+    });
+  }
   return (
     <div className="slide-one-container">
-      <div className="slide-one-content">
+      <div className="slide-one-content" ref={slideOneContent}>
         <div className="small-bulb-image-container left-bulb">
           <img src={smallBulb} alt="small bulb" />
         </div>
@@ -31,7 +44,7 @@ export default function SlideOne() {
           <div>Fly</div>
         </div>
         <div className="bulb-image-container" ref={bulbImageContainer}>
-          <img src={bulbImage} alt="bulb" />
+          <img src={bulbImage} alt="bulb" className="bulb-image" ref={bulbImageRef} />
         </div>
       </div>
     </div>
