63c527338fc9dd4e4e1c5c77f187808c7c1a5979
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-03-30 00:58:09 +0300

    Add animations to slide three

--- src/components/slides/SlideThree.jsx
+++ src/components/slides/SlideThree.jsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import '../../assets/styles/slides/slidethree.scss';
+import { splitLetters } from '../../utils';
 
 export default function SlideThree() {
+  const slideThreeContainer = useRef(null);
+  let letters = null;
+  var lettersArray = [];
+  let timeOutFunctionOne = null;
+  let timeOutFunctionTwo = null;
 
-  //   useEffect(() => showBulb());
-  //   function showBulb() {
-  //     setTimeout(() => {
-  //       digitalTextContainer.current.className += ' show-digital-text';
-  //       digitalImageContainer.current.className += ' show-heart';
-  //     }, 1200);
-  //     setTimeout(() => {
-  //       heartImage.current.classList.remove('imageSpin');
-  //     }, 1800);
-  //   }
+  useEffect(() => {
+    letters = document.getElementsByClassName('letters');
+    timeOutFunctionOne = setTimeout(() => showSlide(), 0.2);
+    handleLettersAnimation();
+    return () => {
+      clearTimeout(timeOutFunctionOne);
+      clearTimeout(timeOutFunctionTwo);
+    };
+  }, []);
+
+  const showSlide = () => {
+    slideThreeContainer.current.classList.add('full-height');
+  };
+
+  /**
+   * Handles overall letter animation
+   * @returns {void}
+   */
+  const handleLettersAnimation = () => {
+    for (var i = 0; i < letters.length; i++) {
+      const spiltLetters = splitLetters(letters[i]);
+      lettersArray.push(...spiltLetters);
+    }
+    for (var i = 0; i < lettersArray.length; i++) {
+      lettersArray[i].className = 'letter behind';
+      animateLetterIn(lettersArray[i], i);
+    }
+  };
+
+  /**
+   * Handles actual animation
+   * @param {HTMLElement} letter - span containing the letter to be animated
+   *  * @param {Number} index - position of the letter in the letters array
+   * @returns {void}
+   */
+  function animateLetterIn(letter, index) {
+    setTimeout(function() {
+      letter.parentElement.style.opacity = 1;
+      letter.className = 'letter in';
+    }, 400 + index * 80);
+  }
+
   return (
-    <div className="slide-three-container">
+    <div className="slide-three-container" ref={slideThreeContainer}>
       <div className="slide-three-banner">
         {/* <div className="btl-image-text-container" ref={digitalTextContainer}>
           <img src={btlTextImage} alt="btl text" />
         </div> */}
-        <div className="split-letters-container">
-          <div>tangible </div>
-          <div>creativity</div>
+        <div className="slide-three-letters">
+          <div className="letters">Tangible</div>
+          &nbsp;
+          <div className="letters">Creativity</div>
         </div>
       </div>
 
