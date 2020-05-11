fd4e3b5ef082f82ae22f19e9699c6622efdb05cf
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-05-12 01:44:39 +0300

    Rename BTL Page to Creative Content

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import Home from './Home';
 import BottomPage from './BottomPage';
 import PartnersPage from './pages/PartnersPage';
 import BrandingPage from './pages/BrandingPage';
-import BTLPage from './pages/BTLPage';
+import CreativeContent from './pages/CreativeContent';
 import DigitalPage from './pages/DigitalPage';
 
 function LandingPage() {
@@ -18,7 +18,7 @@ function LandingPage() {
         <BrandingPage />
       </section>
       <section className="section-three">
-        <BTLPage />
+        <CreativeContent />
       </section>
       <section className="section-four">
         <DigitalPage />
