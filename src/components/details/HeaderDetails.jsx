22ade301a1abae0280cc6c9621b9a9f2b2a0a1a1
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-05-28 18:29:07 +0300

    Add ui ux title to ui ux details page

--- src/components/details/HeaderDetails.jsx
+++ src/components/details/HeaderDetails.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import styled from 'styled-components';
 
-const Container = styled.div`
+const HeaderDetails = styled.div`
   width: 100%;
   height: calc(100vh / 2);
   @media (min-width: 1340px) {
@@ -13,18 +11,11 @@ const Container = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 50%;
+  position: relative;
   ${props => `
       background-image: url(${props.bgImage});
     `}
 `;
 
-function HeaderDetails(props) {
-  const { bgColor, bgImage } = props;
-  return (
-    <Container bgColor={bgColor} bgImage={bgImage}>
-      <div></div>
-    </Container>
-  );
-}
 
 export default HeaderDetails;
--- src/components/details/UIUXDetails.jsx
+++ src/components/details/UIUXDetails.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import Header from '../Header';
 import FooterDetail from './FooterDetail';
 import HeaderDetails from './HeaderDetails';
 import ProjectItem from './ProjectItem';
 
 import { uiUxProjects } from '../../utils/constants';
-import { brandingTextBanner, uiUxDetailsBg } from '../../assets';
+import { uiUxLabel, uiUxMain, uiUxDetailsBg } from '../../assets';
 import '../../assets/styles/details/common.scss';
 import '../../assets/styles/details/uiuxdetails.scss';
+
+const HeaderImage = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderImageContainer = styled.div`
+  flex: 1;
+  & img {
+    width: 100%;
+  }
+`;
 
 const UIUXDetails = () => {
   return (
@@ -16,9 +37,14 @@ const UIUXDetails = () => {
       <Header />
       <div>
         <HeaderDetails bgImage={uiUxDetailsBg}>
-          <div className="details-header-image-container">
-            <img src={brandingTextBanner} alt="text branding" />
-          </div>
+          <HeaderImage>
+            <HeaderImageContainer>
+              <img src={uiUxLabel} alt="text branding" />
+            </HeaderImageContainer>
+            <HeaderImageContainer>
+              <img src={uiUxMain} alt="text branding" />
+            </HeaderImageContainer>
+          </HeaderImage>
         </HeaderDetails>
         <div className="featured-projects-title">
           <h1>
