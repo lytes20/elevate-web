dccbaa26442f845d9a6b87dbefa62471eddee34b
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-05-28 23:31:27 +0300

    Update the latest projects links on landing page

--- src/components/pages/PartnersPage.jsx
+++ src/components/pages/PartnersPage.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import '../../assets/styles/pages/partnerpage.scss';
+
+import { Link } from 'react-router-dom';
+
 import Clients from '../Clients';
 
+import '../../assets/styles/pages/partnerpage.scss';
+
 export default function PartnersPage() {
-  function createData(name, imageUrl) {
-    return { name, imageUrl };
+  function createData(id, name, path, imageUrl) {
+    return { id, name, path, imageUrl };
   }
   const latestProjects = [
     createData(
-      'branding',
+      'one',
+      'UI UX',
+      '/ui-ux',
       'https://images.unsplash.com/photo-1579130589334-31c6414eafdb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'
     ),
     createData(
-      'btl',
+      'two',
+      'Creative Content',
+      '/creative-content',
       'https://images.unsplash.com/photo-1578874619862-55fc955ad118?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
     ),
     createData(
-      'digital',
+      'three',
+      'Social Media management',
+      '/social-media-management',
       'https://images.unsplash.com/photo-1579121477063-4026649ad6d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
     )
   ];
@@ -27,9 +37,9 @@ export default function PartnersPage() {
         <div className="projects-container">
           {latestProjects.map((latestProject, index) => (
             <div key={index} className="project-container">
-              <img src={latestProject.imageUrl} alt={latestProject.name} />
-              <div className={`project-div ${latestProject.name}-div`}>
-                <span>{latestProject.name}</span>
+              <img src={latestProject.imageUrl} alt={latestProject.id} />
+              <div className={`project-div ${latestProject.id}-div`}>
+                <Link to={latestProject.path}>{latestProject.name}</Link>
               </div>
             </div>
           ))}
